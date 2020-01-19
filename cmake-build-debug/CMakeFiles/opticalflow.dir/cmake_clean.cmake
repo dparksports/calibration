2cad9382c725250ce0292da3068b1a19accb2573
Dan Park <dpark.sports@gmail.com>
2020-01-18 17:59:59 -0800

    1st commit

---
+++ cmake-build-debug/CMakeFiles/opticalflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opticalflow.dir/main.cpp.o"
+  "opticalflow"
+  "opticalflow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opticalflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,175 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <opencv2/core/core.hpp>
+
+#include <opencv2/core/eigen.hpp>
+#include <iostream>
+
+namespace calib {
+// Solve system of linear equations using least squares normal equations
+    Eigen::MatrixXf solveLeastSquares(const Eigen::MatrixXf& pts2d, const Eigen::MatrixXf& pts3d);
+
+// Overload of the above function using cv::Mat instead of Eigen
+    cv::Mat solveLeastSquares(const cv::Mat& pts2d, const cv::Mat& pts3d);
+
+// Solve system of linear equations using singular value decomposition
+    Eigen::MatrixXf solveSVD(const Eigen::MatrixXf& pts2d, const Eigen::MatrixXf& pts3d);
+
+// Overload of the above function using cv::Mat instead of Eigen
+    cv::Mat solveSVD(const cv::Mat& pts2d, const cv::Mat& pts3d);
+}; //
+
+
+
+Eigen::MatrixXf calib::solveLeastSquares(const Eigen::MatrixXf& pts2d,
+                                         const Eigen::MatrixXf& pts3d) {
+    assert(pts2d.cols() == pts3d.cols() && pts2d.rows() == 2 && pts3d.rows() == 3);
+    // Set up A and b matrices.
+    const size_t rows = pts3d.cols() * 2;
+    const size_t cols = 11;
+    Eigen::MatrixXf A(rows, cols);
+    Eigen::MatrixXf b(rows, 1);
+
+    // Build A and b matrices
+    for (int i = 0; i < rows; i += 2) {
+        float X = pts3d(0, i / 2);
+        float Y = pts3d(1, i / 2);
+        float Z = pts3d(2, i / 2);
+        float x = pts2d(0, i / 2);
+        float y = pts2d(1, i / 2);
+        A.row(i) << X, Y, Z, 1, Eigen::MatrixXf::Zero(1, 4), -x * X, -x * Y, -x * Z;
+        A.row(i + 1) << Eigen::MatrixXf::Zero(1, 4), X, Y, Z, 1, -y * X, -y * Y, -y * Z;
+        b.row(i) << x;
+        b.row(i + 1) << y;
+    }
+
+    // Solve least squares
+    Eigen::MatrixXf sol = (A.transpose() * A).ldlt().solve(A.transpose() * b);
+    // Append a 1 to the end since scale is constant
+    sol.conservativeResize(sol.rows() + 1, sol.cols());
+    sol(sol.rows() - 1, 0) = 1;
+    return sol;
+}
+
+cv::Mat calib::solveLeastSquares(const cv::Mat& pts2d, const cv::Mat& pts3d) {
+    // Wrap OpenCV matrices in Eigen::Map
+    Eigen::MatrixXf eigenPts2d, eigenPts3d;
+    cv::cv2eigen(pts2d, eigenPts2d);
+    cv::cv2eigen(pts3d, eigenPts3d);
+
+    auto eigenSol = solveLeastSquares(eigenPts2d, eigenPts3d);
+    cv::Mat cvSol;
+    cv::eigen2cv(eigenSol, cvSol);
+    return cvSol;
+}
+
+Eigen::MatrixXf calib::solveSVD(const Eigen::MatrixXf& pts2d, const Eigen::MatrixXf& pts3d) {
+    assert(pts2d.cols() == pts3d.cols() && pts2d.rows() == 2 && pts3d.rows() == 3);
+    // Set up A and b matrices.
+    const size_t rows = pts3d.cols() * 2;
+    const size_t cols = 12;
+    Eigen::MatrixXf A(rows, cols);
+
+    // Build A and b matrices
+    for (int i = 0; i < rows; i += 2) {
+        float X = pts3d(0, i / 2);
+        float Y = pts3d(1, i / 2);
+        float Z = pts3d(2, i / 2);
+        float x = pts2d(0, i / 2);
+        float y = pts2d(1, i / 2);
+        A.row(i) << X, Y, Z, 1, Eigen::MatrixXf::Zero(1, 4), -x * X, -x * Y, -x * Z, -x;
+        A.row(i + 1) << Eigen::MatrixXf::Zero(1, 4), X, Y, Z, 1, -y * X, -y * Y, -y * Z, -y;
+    }
+
+    // Compute the orthogonal matrix of eigenvectors of A_T*A
+    Eigen::MatrixXf eigenvectors = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).matrixV();
+    // Get the eigenvector with the smallest eigenvalue (the last column of the V matrix)
+    Eigen::MatrixXf smallest = eigenvectors.col(eigenvectors.cols() - 1);
+    // std::cout << "sol = \n" << smallest << std::endl;
+    return smallest;
+}
+
+cv::Mat calib::solveSVD(const cv::Mat& pts2d, const cv::Mat& pts3d) {
+    // Convert OpenCV matrices to Eigen matrices
+    Eigen::MatrixXf eigenPts2d, eigenPts3d;
+    cv::cv2eigen(pts2d, eigenPts2d);
+    cv::cv2eigen(pts3d, eigenPts3d);
+
+    auto eigenSol = solveSVD(eigenPts2d, eigenPts3d);
+    cv::Mat cvSol;
+    cv::eigen2cv(eigenSol, cvSol);
+    return cvSol;
+}
+// Project a set of points from 3D space to the 2D image plane
+cv::Mat project3D(const cv::Mat& projMat, const cv::Mat& pt3d) {
+    // No assert here because matrix multiplication in OpenCV already has one
+    cv::Mat projected = projMat * pt3d;
+    // Last value in projected is the homogeneous value - divide by this to scale correctly to an
+    // inhomogeneous point
+    for (size_t col = 0; col < projected.cols; col++) {
+        float s = projected.at<float>(2, col);
+        projected.col(col) = projected.col(col) / s;
+    }
+
+    return projected;
+}
+
+void runProblem1a() {
+    cv::Mat _picA, _picB, _picANorm, _pts3D, _pts3DNorm;
+
+    // Get the last 3D normalized point so we can check our m matrix later
+    cv::Mat lastPt3D = _pts3DNorm.col(_pts3DNorm.cols - 1);
+    lastPt3D.push_back(1.f);
+    cv::Mat lastPt2D = _picANorm.col(_picANorm.cols - 1);
+
+    // Row-vector version of the last point so that we can print it more easily
+    cv::Mat lastPt3D_T;
+    cv::transpose(lastPt3D, lastPt3D_T);
+
+    // Compute projection matrix
+    {
+        auto sol = calib::solveLeastSquares(_picANorm, _pts3DNorm);
+        cv::Mat params = sol.reshape(0, 3);
+
+        cv::Mat projection = project3D(params, lastPt3D);
+
+        // Compute residual
+        double residual = cv::norm(projection.rowRange(0, 2), lastPt2D);
+
+        // Transpose of projected point (just for logging)
+        cv::Mat projection_T;
+        cv::transpose(projection, projection_T);
+//        logger->info("Calibration parameters (using normal least squares):\n{}\nProjected "
+//                     "3D point\n{}\nto 2D point\n{}\nResidual = {}",
+//                     FormattedMat(params),
+//                     lastPt3D_T,
+//                     projection_T,
+//                     residual);
+    }
+
+    {
+        auto sol = calib::solveSVD(_picANorm, _pts3DNorm);
+        cv::Mat params = sol.reshape(0, 3);
+
+        cv::Mat projection = project3D(params, lastPt3D);
+
+        // Compute residual
+        double residual = cv::norm(projection.rowRange(0, 2), lastPt2D);
+
+        // Transpose of projected point (just for logging)
+        cv::Mat projection_T;
+        cv::transpose(projection, projection_T);
+//        logger->info("Calibration parameters (using singu    Config config(CONFIG_FILE_PATH);
+//lar value decomposition):\n{}\nProjected "
+//                     "3D point\n{}\nto 2D point\n{}\nResidual = {}",
+//                     FormattedMat(params),
+//                     lastPt3D_T,
+//                     projection_T,
+//                     residual);
+    }
+}
+
+int main() {
+    runProblem1a();
+}
